b29a7cb8e09d0708d3f95ae82e4ca30b6d009098
araggohnxd <matheusnagato300@gmail.com>
2022-02-28 19:18:29 -0300

    Exercício 00 C07

---
+++ c/c_07/ex00/ft_strdup.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maolivei <maolivei@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/22 14:41:25 by maolivei          #+#    #+#             */
+/*   Updated: 2022/02/22 16:05:14 by maolivei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int	ft_strlen(char *str)
+{
+	int	size;
+
+	size = 0;
+	while (*str++)
+		size++;
+	return (size);
+}
+
+char	*ft_strcpy(char *dest, char *src)
+{
+	int	i;
+
+	i = 0;
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+char	*ft_strdup(char *src)
+{
+	char	*dup;
+	int		len;
+
+	dup = 0;
+	len = ft_strlen(src);
+	if (len == 0)
+		return (dup);
+	dup = malloc(len + 1);
+	ft_strcpy(dup, src);
+	return (dup);
+}
